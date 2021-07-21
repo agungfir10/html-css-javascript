5d0d38333098d2744e39c19e8f7c37f3ff20bc3a
Agung Firmansyah <agungfirid@gmail.com>
2021-07-21 22:15:26 +0700

    Expanding Card 1 : Update Product Flex Anda Zoom Animation Background

--- expanding-card/expanding-card-1.js
+++ expanding-card/expanding-card-1.js
@@ -1,21 +1,20 @@
 const panels = document.querySelectorAll(".panel");
 const headingJumbotron = document.querySelector("#jumbotron h2");
-const layers = document.querySelectorAll(".layer-jumbotron");
 
-layers.forEach((layer, i, arr) => {
-  layer.addEventListener("mouseover", () => {
+panels.forEach((panel, i, arr) => {
+  panel.addEventListener("mouseover", () => {
     removeActiveClasses();
     panels[i].classList.add("active");
     headingJumbotron.style.visibility = "hidden";
   });
-  layer.addEventListener("mouseout", () => {
+  panel.addEventListener("mouseout", () => {
     removeActiveClasses();
     headingJumbotron.style.visibility = "visible";
   });
 });
 
 function removeActiveClasses() {
-  layers.forEach((layer, i, arr) => {
-    panels[i].classList.remove("active");
+  panels.forEach((panel, i, arr) => {
+    panel.classList.remove("active");
   });
 }
